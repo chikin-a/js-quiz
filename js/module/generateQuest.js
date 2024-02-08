3aa8cea1f811b9f355a3a37d8b3f2b7395bf0395
anton-chikin <chikin.a.dev@gmail.com>
2024-02-08 22:22:03 +0200

    refactoring generateQuest

--- js/module/generateQuest.js
+++ js/module/generateQuest.js
@@ -1,22 +1,13 @@
 const generateQuest = (arr, i) => {
-  const quizBody = document.querySelector('.quiz-body')
   const { type, quest } = arr[i]
   const { answers } = arr[i]
 
-  quizBody.innerHTML = `
-    <label for="quest" class="question">${quest}</label>
-    <div class="answers">
-      <div class="answers-item">
-       <input name="quest" value="a" type="${type}" /> ${answers[0]}
-      </div>
-      <div class="answers-item">
-       <input name="quest" value="b" type="${type}" /> ${answers[1]}
-      </div>
-      <div class="answers-item">
-       <input name="quest" value="c" type="${type}" /> ${answers[2]}
-      </div>
-    </div>
-  `
+  const question = (document.querySelector('.question').textContent = quest)
+  const answersItems = document.querySelectorAll('.answers-item')
+
+  answersItems[0].innerHTML = `<input name="quest" value="a" type="${type}" /> ${answers[0]}`
+  answersItems[1].innerHTML = `<input name="quest" value="b" type="${type}" /> ${answers[1]}`
+  answersItems[2].innerHTML = `<input name="quest" value="c" type="${type}" /> ${answers[2]}`
 }
 
 export default generateQuest
