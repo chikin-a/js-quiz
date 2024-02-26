d55cd6480ea8eb2f9cabd7fa2f7ce6f437f85a7c
anton-chikin <chikin.a.dev@gmail.com>
2024-02-26 20:20:01 +0200

    genaration result

--- js/module/generationResult.js
+++ js/module/generationResult.js
@@ -2,10 +2,20 @@ import { DB } from './DB.js'
 
 export const generationResult = (s) => {
   const quiz = document.querySelector('.quiz')
+  const container = quiz.querySelector('.container')
   const quizForm = document.querySelector('.quiz-form')
+  const quizResult = document.createElement('div')
+  const resultSolution = document.createElement('p')
+  quizResult.classList.add('quiz-result')
+  resultSolution.classList.add('result-solution')
 
   quizForm.classList.add('hide')
+
+  DB.forEach((el) => (quizResult.innerHTML += `<h3>${el.question}</h3>`))
+
   setTimeout(() => {
-    quiz.innerHTML = `<p class="score">${s}/${DB.length}</p> `
+    // quiz.innerHTML = `<p class="score">${s}/${DB.length}</p> `
+    quizForm.remove()
+    container.appendChild(quizResult)
   }, 400)
 }
